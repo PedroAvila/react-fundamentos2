28ffcd4ba5f60e4f3aa52c7acaf058da38157855
PedroAvila <minimainor_austin@hotmail.com>
2021-05-18 09:11:45 -0500

    Controlar los datos de entrada con prop-types

--- src/App.js
+++ src/App.js
@@ -1,72 +1,45 @@
 
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
+class Profile extends Component {
 
-class PortalModal extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    bio: PropTypes.string,
+    email: PropTypes.string,
+    age: PropTypes.number
+  }
+
   render(){
 
-    if(!this.props.visible){
-      return null;
-    }
+    const { name, bio, email } = this.props
 
-    const styles = {
-      width: "100%",
-      height: "100%",
-      position: "absolute",
-      top: "0",
-      left: "0",
-      background: "linear-gradient(to top right, #667eea, #764ba2)",
-      opacity: "0.95",
-      color: "#FFF"
-    }
-
-    return ReactDOM.createPortal((
-      <div style={styles}>
-        { this.props.children }
+    return(
+      <div>
+        <h1>{ name }</h1>
+        <p>
+          { bio }
+        </p>
+        <a href={`mailto:${ email }`}>
+          { email }
+        </a>
       </div>
-    ), document.getElementById("modal-root"))
+    )
   }
 }
 
 
 class App extends Component {
 
-  state = {
-    visible: false,
-    num: 0
-  }
-
-  componentDidMount () {
-    setInterval(() =>{
-      this.setState(state=>({
-        num: state.num + 1
-      }))
-    }, 1000)
-  }
-
-  mostrar = () => {
-    this.setState({ visible: true })
-  }
-
-  cerrar = () => {
-    this.setState({ visible: false })
-  }
-
   render(){
     return(
       <div>
-        <button onClick={this.mostrar}>
-          Mostrar
-        </button>
-        <PortalModal visible={ this.state.visible }>
-          <button onClick={ this.cerrar }>
-            Cerrar
-          </button>
-          <h1>
-            Hola desde un PortalModal { this.state.num }
-          </h1>
-        </PortalModal>
+        <Profile
+          name="Gerardo Gallegos"
+          bio="Soy un desarrollador FullStack"
+          email="my-email@mail.com"
+        />
       </div>
     )
   }
